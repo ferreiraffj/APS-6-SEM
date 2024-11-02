e9a591974aadb5a2b57d6c03a95efd17b56c0489
ferreiraffj <felipeffjcontato@gmail.com>
2024-11-01 22:45:21 -0300

    feature: inclusao de activity dialog para edicao dos usuarios

--- PIVC_BIOMETRIC_APP/app/src/main/java/com/aps/pivc_biometric_app/ManageUserActivity.java
+++ PIVC_BIOMETRIC_APP/app/src/main/java/com/aps/pivc_biometric_app/ManageUserActivity.java
@@ -2,18 +2,24 @@ package com.aps.pivc_biometric_app;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
@@ -25,6 +31,9 @@ public class ManageUserActivity extends AppCompatActivity {
     private Button buttonSearch;
     private TextView textViewResult;
     private FirebaseFirestore db;
+    private String userEmail;
+    private int nvlPermission;
+    private String userId; // Para armazenar o ID do documento do usuário
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -41,8 +50,15 @@ public class ManageUserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 searchUserByEmail();
+                textViewResult.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showEditPermissionDialog(nvlPermission);
+                    }
+                });
             }
         });
+
     }
 
     @SuppressLint("SetTextI18n")
@@ -64,7 +80,7 @@ public class ManageUserActivity extends AppCompatActivity {
                             userFound = true;
                             String userEmail = document.getString("email");
                             int nvlPermission = Objects.requireNonNull(document.getLong("permissionLevel")).intValue();
-                            // Aqui você pode pegar outras informações do usuário, se necessário
+                            userId = document.getId();
                             textViewResult.setText("Usuário encontrado: " + userEmail + " (. Nível: " + nvlPermission + ")");
                         }
                         if (!userFound) {
@@ -75,4 +91,79 @@ public class ManageUserActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void showEditPermissionDialog(int currentPermissionLevel){
+        View dialogView = getLayoutInflater().inflate(R.layout.activity_user_detail, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        // Configuração do Spinner
+         @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Spinner spinnerPermissionLevels = dialogView.findViewById(R.id.spinnerPermissionLevels);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.permission_levels, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerPermissionLevels.setAdapter(adapter);
+
+        // Define o valor atual do Spinner
+        spinnerPermissionLevels.setSelection(currentPermissionLevel - 1);
+
+        // Configuração do botão de atualização
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button buttonUpdatePermission = dialogView.findViewById(R.id.buttonUpdatePermission);
+
+        buttonUpdatePermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int newPermissionLevel = Integer.parseInt((String) spinnerPermissionLevels.getSelectedItem());
+                updatePermissionLevel(newPermissionLevel);
+            }
+        });
+
+        builder.setTitle("Editar nível de permissão")
+                .setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss())
+                .show();
+    }
+
+    private void updatePermissionLevel(int newPermissionLevel){
+        if (userId == null){
+            Toast.makeText(this, "ID do usuário não encontrado", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        DocumentReference userRef = db.collection("users").document(userId);
+        userRef.update("permissionLevel", newPermissionLevel)
+                .addOnSuccessListener(aVoid ->{
+                    Toast.makeText(ManageUserActivity.this, "Permissão atualizada com sucesso", Toast.LENGTH_SHORT).show();
+                    textViewResult.setText(String.valueOf(newPermissionLevel));
+                })
+                .addOnFailureListener(e ->{
+                    Toast.makeText(ManageUserActivity.this, "Erro ao atualizar permissão" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PIVC_BIOMETRIC_APP/app/src/main/java/com/aps/pivc_biometric_app/UserDetailActivity.java
@@ -0,0 +1,31 @@
+package com.aps.pivc_biometric_app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UserDetailActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
